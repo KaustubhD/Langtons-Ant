b9567478d4c77db25c2dd7a757f1a332b9f594f9
KaustubhD <kaustubhd287@gmail.com>
2018-11-29 19:05:16 +0530

    Cleanup

--- langton.js
+++ langton.js
@@ -1,8 +1,8 @@
 let canvas = document.getElementById('can')
 let ctx = canvas.getContext('2d')
 
-let w = 600
-let h = 600
+let w = 450
+let h = 450
 
 canvas.width = w
 canvas.height = h
@@ -18,7 +18,6 @@ let pixArray = Array.from({ length: w }, () => new Array(h).fill(0))
 //   pixArray[i] = new Array(h).fill(0)
 // }
 // pixArray = pixArray.map(el => { el = new Array(h).fill(0) })
-console.log(pixArray)
 let posX = w / 2
 let posY = h / 2
 
@@ -30,7 +29,6 @@ const LEFT = 3
 let state = UP
 
 function forward(){
-  // console.log(`Got state ${state}`)
   switch(state){
     case 0:
       posY--
@@ -49,70 +47,46 @@ function forward(){
   else if(posX < 0) posX = w - 1
   if(posY > h - 1) posY = 0
   else if(posY < 0) posY = h - 1
-  // posX = posX % w
-  // posY = posY % h
 }
 
 function turnClockwise(){
-  // console.log(`called Clock with ${state}`)
   state = state + 1
   if(state > LEFT) state = UP
-  // state = (state + 1) > LEFT ? UP : state + 1
 }
 
 function turnAntiClockwise(){
-  // console.log(`called Anti with ${state}`)
   state = state - 1
   if(state < UP) state = LEFT
-  // state = (state - 1) < UP ? LEFT : state - 1
-  // console.log(`sent with ${state}`)
 }
 
-// pixArray[posX][posY] = 1
-
-let acInac = 0
-let col = true
+let currentPixel = 0
 function draw(){
-  // console.log(pixArray)
-  // console.log(`\n\nX is ${posX} and Y is ${posY}`)
-  acInac = pixArray[posX][posY]
-  // console.log(acInac)
-  if(acInac == 0){
-    // console.log(`Found White ${posX} and Y is ${posY} and current state is ${state}`)
+  currentPixel = pixArray[posX][posY]
+  if(currentPixel == 0){
     turnClockwise()
     
     pixArray[posX][posY] = 1
-    // console.log(`X is ${posX} and Y is ${posY}`)
     ctx.fillStyle = '#fff'
     forward()
   }
-  else if(acInac == 1){
-    // console.log(`Found Black ${posX} and Y is ${posY}  and current state is ${state}`)
+  else if(currentPixel == 1){
     turnAntiClockwise()
     pixArray[posX][posY] = 0
     ctx.fillStyle = '#000'
     forward()
   }
-  // console.log(pixArray[posX][posY])
-  // pixPos = i + (w * j)
   ctx.fillRect(posX, posY, 1, 1)
-  
-  
-  // for(let i = 0; i < w; i++){
-  //   for(let j = 0; j < h; j++){
-
-  //   }
-  // }
-
-
 }
+
 
 let x = setInterval(function(){
   for(let i = 0; i < 150; i++){
     draw()
   }
 }, 60)
-// let limit = 11000
+// let limit = 200
 // for(let k = 0; k < limit; k++){
-//   draw()
+//   for(let i = 0; i < 150; i++){
+//         draw()
+//       }
 // }
